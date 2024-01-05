3707aadaf9c334ac525f55f51804e56f895f9869
picoloman6 <a2328b@naver.com>
2024-01-05 20:59:42 +0900

    리프노드 네번째

--- 240105_리프노드의개수구하기.js
+++ 240105_리프노드의개수구하기.js
@@ -1,37 +1,31 @@
-const readline = require('readline');
+// const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
-const input = [];
+// const input = [];
 
-rl.on('line', (line) => {
-  input.push(line);
-}).on('close', () => {
-  solution(input);
-});
+// rl.on('line', (line) => {
+//   input.push(line);
+// }).on('close', () => {
+//   solution(input);
+// });
 
 const dfs = (list, ban, check, n, answer) => {
+  let child = 0;
   check[n] = true;
-  if (n === ban) {
-    return;
+
+  for (const next of list[n]) {
+    if (!check[next] && next !== ban) {
+      dfs(list, ban, check, next, answer);
+      child++;
+    }
   }
 
-  if (list[n].length === 0) {
+  if (child === 0) {
     answer[0]++;
-  } else {
-    let child = 0;
-    for (const next of list[n]) {
-      if (!check[next]) {
-        dfs(list, ban, check, next, answer);
-        child++;
-      }
-    }
-    if (child === 0) {
-      answer[0]++;
-    }
   }
 };
 
@@ -54,10 +48,12 @@ const solution = (input) => {
     }
   }
 
-  console.log(list);
-  dfs(list, ban, check, start, answer);
-
-  console.log(answer[0]);
+  if (ban === start) {
+    console.log(0);
+  } else {
+    dfs(list, ban, check, start, answer);
+    console.log(answer[0]);
+  }
 };
 
 const p1 = ['5', '-1 0 0 1 1', '2'];
@@ -65,4 +61,4 @@ const p2 = ['5', '-1 0 0 1 1', '1'];
 const p3 = ['5', '-1 0 0 1 1', '0'];
 const p4 = ['9', '-1 0 0 2 2 4 4 6 6', '4'];
 
-solution(p1);
+solution(p4);
